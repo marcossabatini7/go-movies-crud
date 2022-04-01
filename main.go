f7966fe327146499c83d3850296725e0b62308b1
Marcos Sabatini <marcossabatini7@gmail.com>
2022-03-31 22:35:15 -0300

    Init server

--- main.go
+++ main.go
@@ -1,6 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
-	println("Teste")
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+var movies []Movie
+
+func main() {
+	PORT := 8000
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{FirstName: "John", LastName: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "112366", Title: "Movie Two", Director: &Director{FirstName: "Carlos", LastName: "Escobar"}})
+
+	// r.HandleFunc("/movies", getMovies).Methods("GET")
+	// r.HandleFunc("/movies/{id", getMovie).Methods("GET")
+	// r.HandleFunc("/movies", createMovie).Methods("POST")
+	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	// r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port %d\n", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), r))
 }
